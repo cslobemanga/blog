552fb5aadb86ceae590e407c03ba61af83e6db95
Charles Lobe-Manga <charles@lobe-manga.com>
2017-09-10 11:28:51 +0200

    Cleaning up

--- app/config/settings.php
+++ app/config/settings.php
@@ -25,8 +25,8 @@ define( 'user_config', array(
         // DB-Verbindung
         'db.host'               =>  'localhost',
         'db.dbname'             =>  'db_mvc',
-        'db.user'               =>  '',
-        'db.password'           =>  '',
+        'db.user'               =>  'homestead',
+        'db.password'           =>  'secret',
     
         'default_user_role'     =>  0,
         'default_user_status'   =>  1,
@@ -41,26 +41,15 @@ define( 'user_config', array(
         'smtp.username'         => '',
         'smtp.password'         => '',
         'smtp.debug'            => 2,
-        'smtp.sender.email'     => 'charles@lobe-manga.com',
-        'smtp.sender.name'      => 'Charles Lobe-Manga',
+        'smtp.sender.email'     => 'testuser@test.com',
+        'smtp.sender.name'      => '',
     
         'mail.subject'          => 'Standard PHPMailer test message',
         'mail.addresses'        => [
                                     [ 'email' => 'lobe-manga@web.de', 
-                                      'name'  => 'Charles Lobe-Manga' ],
-            
-                                    [ 'email' => 'elsa-gogia@web.de', 
-                                      'name'  => 'Eliza Gogia']
-                                   ],
-        'mail.standard.message' => 'Sehr geehrte Damen und Herren,<br/><br/>' .
-            'im Anhang dieser E-Mail finden Sie das Protokoll zum heutigen Test' .
-            'der produktiven Systeme.<br/><br/>Sollten Sie Fragen dazu haben, ' .
-            'stehen wir Ihnen gerne zur Verfügung.<br/><br/>Wir wünschen Ihnen ' . 
-            'einen angenehmen und erfolgreichen Tag und viel Spaß bei der Arbeit ' . 
-            'mit AdWorks.<br/><br/>Ihr<br/>financeTec Support Team<br/><br/>' . 
-            'financeTec AG<br/>Korngasse 2<br/>67547 Worms<br/><br/>' . 
-            'Tel. 069-401570200<br/>Fax. 069-401570222<br/>' . 
-            'Mail: support@financetec.de',
+                                      'name'  => 'Charles Lobe-Manga' ]
+                                    ],
+        'mail.standard.message' => 'Sehr geehrte Damen und Herren <br/>',
     )
 );
 
